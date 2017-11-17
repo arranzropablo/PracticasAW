143bfd4d42d5a9cfb28f318c865eb8240e891a08
caminosajalberto <jalbercamino@gmail.com>
2017-11-17 17:57:50 +0100

    changed

--- Practica1/database_init.sql
+++ Practica1/database_init.sql
@@ -1,8 +1,20 @@
+CREATE TABLE `usuarios` (
+  `email` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `password` varchar(16) DEFAULT NULL,
+  `sexo` char(1) NOT NULL,
+  `fecha_nacimiento` varchar(12) DEFAULT NULL,
+  `imagen_perfil` varchar(100) DEFAULT NULL,
+  `puntos` int(6) DEFAULT '0',
+  PRIMARY KEY (`email`)
+) 
 
-
-create table amigos(origen varchar(50), 
-								destino varchar(50), 
-                                pendiente boolean, 
-                                primary key (origen,destino), 
-                                foreign key (origen) references usuarios(email),
-                                foreign key (destino) references usuarios(email));
+CREATE TABLE `amigos` (
+  `origen` varchar(50) NOT NULL DEFAULT '',
+  `destino` varchar(50) NOT NULL DEFAULT '',
+  `pendiente` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`origen`,`destino`),
+  KEY `destino` (`destino`),
+  CONSTRAINT `amigos_ibfk_1` FOREIGN KEY (`origen`) REFERENCES `usuarios` (`email`),
+  CONSTRAINT `amigos_ibfk_2` FOREIGN KEY (`destino`) REFERENCES `usuarios` (`email`)
+) 
